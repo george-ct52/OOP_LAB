ffafb8db4e88d726dbea6e198ac7e1e326f8f084
George C Thomas <61283815+george-ct52@users.noreply.github.com>
2023-11-24 23:15:14 +0530

    Update perimeter.java

--- perimeter.java
+++ perimeter.java
@@ -1,24 +1,19 @@
 
+class Perimeter {
+    int length, breadth, radius;
 
-class perimeter {
-    int length,breadth,r;
-    perimeter(int length,int breadth)
-    {
-        float perimeter=2*(length+breadth);
-        System.out.println("The Perimeter of the rectangle ="+perimeter);
-        
-    }
-    perimeter(int radius)
-    {
-        double perimeter =2*3.14*radius;
-        System.out.println("The Perimeter of the circle ="+perimeter);
-    }
-    public static void main(String args[])
-    {
-       perimeter circle = new perimeter(4) ;
-
-        perimeter rect = new perimeter(10,20); 
+    Perimeter(int length, int breadth) {
+        double perimeter = 2 * (length + breadth);
+        System.out.println("The Perimeter of the rectangle = " + perimeter);
     }
 
+    Perimeter(int radius) {
+        double perimeter = 2 * 3.14 * radius;
+        System.out.println("The Perimeter of the circle = " + perimeter);
+    }
 
+    public static void main(String args[]) {
+        Perimeter circle = new Perimeter(4);
+        Perimeter rect = new Perimeter(10, 20);
+    }
 }
